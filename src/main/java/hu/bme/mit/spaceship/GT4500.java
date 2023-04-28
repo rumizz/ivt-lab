31aed4b50f7072cb8ffc2863b9440318f996471b
rumizz <rumi.zoard@gmail.com>
2023-04-28 10:51:22 +0200

    Update GT4500.java

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,12 +78,12 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
 
         break;
     }
 
-    return firingSuccess;
+    return  ;
   }
 
 }
